f92fbd9bb0e87d11ee7ef486281bb68b86498e69
Rose <miniusbhater@gmail.com>
2025-05-27 16:13:24 +0200

    Add files via upload

---
+++ test.c
@@ -0,0 +1,15 @@
+//
+//  test.c
+//  CTest
+//
+//  Created by rose on 27/05/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    printf("I dont know what im doing :sob:\n");
+    
+    return 0;
+}
