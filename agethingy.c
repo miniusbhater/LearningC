f00c40fb3df85886a95f6092fc765cedbf6e3527
Rose <miniusbhater@gmail.com>
2025-05-27 16:17:42 +0200

    Add files via upload

---
+++ agethingy.c
@@ -0,0 +1,18 @@
+//
+//  test.c
+//  CTest
+//
+//  Created by rose on 27/05/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Enter your age:\n");
+    int age;
+    scanf("%d", &age);
+    printf("age: = %d", age);
+    printf("\n");
+    
+    return 0;
+}
